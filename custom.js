f2e17a7d58d06df3c8fec717eae3c05772d10565
Uttam <90562707+codewithuttam@users.noreply.github.com>
2022-08-04 18:15:03 +0530

    Data type, differnce between let, var and const

--- custom.js
+++ custom.js
@@ -1,3 +1,64 @@
-let age = 28;
-//
-if (age === 28) console.log("Yes i am " + age + " Years old");
+// let myFirstName= 'Ravi';
+
+
+// let x='Raj';
+// let X = "Ravi";
+//**************Data Type******************
+/*  
+ let age =28;
+ let firstName='Raj';
+ let learningJavacript=true;
+ let x;
+ console.log(typeof age);
+ console.log(typeof firstName);
+ console.log(typeof learningJavacript);
+ console.log(x);
+ console.log(typeof x);
+ x=25;
+console.log(typeof x);
+console.log(typeof null);
+*/
+
+
+/*=========let var and const==========================*/
+
+
+let age= 28;
+age =25;
+console.log(age);
+const firstName='Raj';
+function calAge()
+{
+    if(true)
+    {
+       var age =28; 
+       const firstName='Raj';
+       console.log(firstName);
+      
+    }
+     console.log(age);
+    
+}
+calAge();
+console.log(age);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
